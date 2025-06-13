af43f26858a81d6803d7c3b11ec99b4953294117
Oleh <tzymbala.oleg@gmail.com>
2025-06-13 13:29:16 +0300

    New functional - add the value of the next arg to the prev one and print it

--- positional.sh
+++ positional.sh
@@ -5,3 +5,13 @@ for i in "$@";do
 	echo "Arg$counter: $i "
 	(( counter++ ))
 done
+
+args=("$@")
+num_args=${#args[@]}
+
+for ((i=0; i<num_args; i++));do
+	next_index=$(((i+1)%num_args))
+	sum=$((args[i]+args[next_index]))
+	echo "$sum"
+done
+
